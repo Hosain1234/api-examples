029320a84177b331753190daf45bc3fd0612bbec
hosain1234 <fahim7787@gmail.com>
2022-02-27 15:03:29 +0600

    Buddy details is updated with name and email

--- js/buddy.js
+++ js/buddy.js
@@ -3,14 +3,17 @@ const loadBuddy = () => {
     .then(res => res.json())
     .then(data => getBuddy(data))
 };
+loadBuddy();
 const getBuddy = (oneBuddy) => {
     const displayBuddy = oneBuddy.results;
-    const buddyID = document.getElementById('buddy');
+    const buddyDiv = document.getElementById('buddy');
     for(const buddy of displayBuddy){
-        console.log(displayBuddy);
         const p = document.createElement('p');
-        p.innerText = oneBuddy.email;
-        buddyID.appendChild(p);
+        p.innerHTML = `
+        <h3>Name : ${buddy.name.first} ${buddy.name.last}</h3>
+        <p>Email: ${buddy.email}</p>
+        `;
+        buddyDiv.appendChild(p);
     }
     // buddyID.innerText = oneBuddy.results;
 }
